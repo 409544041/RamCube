636e2932eec3cc957f24e52b52180770e0aeaa71
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-09-09 14:49:23 +0200

    ADD: Sceneloader script ADD: canvas + text displaying level number in gameplayscreen. prefabbed to corecomponents

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+	//Config parameters
+	[SerializeField] float loadDelay;
+	[SerializeField] Text levelText;
+
+	private void Start() 
+	{
+		levelText.text = SceneManager.GetActiveScene().name;
+	}
+
+	private void Update()
+	{
+		RestartLevel();
+	}
+
+	private static void RestartLevel()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
