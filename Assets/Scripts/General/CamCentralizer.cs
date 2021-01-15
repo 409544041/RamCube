da08c1adfe67ce5938db98c8b382d1aabed8ca23
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2021-01-15 12:06:36 +0100

    EDIT: cam centralizer is set to center of level and camera is set to focus on that point. Levels will now always be at center of camera

--- Assets/Scripts/General/CamCentralizer.cs
+++ Assets/Scripts/General/CamCentralizer.cs
@@ -5,22 +5,34 @@ using UnityEngine;
 
 namespace Qbism.General
 {
-	[ExecuteInEditMode]
 	public class CamCentralizer : MonoBehaviour
 	{
 		//Config parameters
-		[SerializeField] Camera cam;
+		[SerializeField] Camera cam = null;
+		[SerializeField] Vector3 camValues = new Vector3 (0, 0, 0);
 
 		//States
 		float highest, lowest, leftest, rightest;
 		bool firstValueAssigned = false;
+		public bool inPosition { get; private set; } = false;
 
 		void Start()
-		{
+		{	
 			PositionCenterPoint();
+			PositionCam();
 		}
 
 		private void PositionCenterPoint()
+		{
+			FindEdgeCubes();
+
+			float zPos = lowest + (highest - lowest) / 2;
+			float xPos = leftest + (rightest - leftest) / 2;
+
+			transform.position = new Vector3(xPos, 0, zPos);
+		}
+
+		private void FindEdgeCubes()
 		{
 			CubeHandler handler = FindObjectOfType<CubeHandler>();
 
@@ -41,11 +53,11 @@ namespace Qbism.General
 				if (cube.Value.transform.position.x < leftest) leftest = cube.Value.transform.position.x;
 				if (cube.Value.transform.position.x > rightest) rightest = cube.Value.transform.position.x;
 			}
+		}
 
-			float zPos = lowest + (highest - lowest) / 2;
-			float xPos = leftest + (rightest - leftest) / 2;
-
-			transform.position = new Vector3(xPos, 0, zPos);
+		private void PositionCam()
+		{
+			cam.transform.position = transform.position + camValues;
 		}
 	}
 }
