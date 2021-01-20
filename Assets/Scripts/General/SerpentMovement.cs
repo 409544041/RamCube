38b4e3db80ea31975c3bf78acda69d2b24c7ed9d
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2021-01-20 17:25:57 +0100

    EDIT: using new serpent movement script that works better

--- Assets/Scripts/General/SerpentMovement.cs
+++ Assets/Scripts/General/SerpentMovement.cs
@@ -1,120 +1,86 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿ 
 using UnityEngine;
-using Dreamteck.Splines;
-using MoreMountains.Feedbacks;
+using System.Collections;
+using System.Collections.Generic;
 using Qbism.Cubes;
-
+ 
 namespace Qbism.General
 {
 	public class SerpentMovement : MonoBehaviour
 	{
-		//Config paramters
-		[SerializeField] Transform[] bodyParts;
-		[SerializeField] float minDistance = 1.25f;
-		[SerializeField] float slowDownValue = .25f;
-		[SerializeField] MMFeedbacks overshootJuice = null;
-
-		//Cache
-		SplineFollower follower;
+		//Config parameters
+		[SerializeField] Transform head = null;
+		[SerializeField] Transform[] segments = null;
+		[SerializeField] float segmentSpacing = 1f; 
 
 		//States
-		float dis;
-		Transform curBodyPart;
-		Transform PrevBodyPart;
-		Vector3 lastPos;
-		float moveSpeed = 0;
-		bool isSlowing = false;
-
-		private void Awake() 
-		{
-			follower = GetComponent<SplineFollower>();
-		}
+		List<Vector3> breadcrumbs = null;
 
 		void Start()
 		{
-			// foreach (Transform bodyPart in bodyParts)
-			// {
-			// 	bodyPart.GetComponent<MeshRenderer>().enabled = false;
-			// }
-
-			StartCoroutine(CalcVelocity());
+			breadcrumbs = new List<Vector3>();
+			breadcrumbs.Add(head.position);
+			for (int i = 0; i < segments.Length; i++) 
+				breadcrumbs.Add(segments[i].position);
 		}
 
 		void Update()
 		{
-			if(isSlowing) SlowDown();
 
-			Move();
-		}
+			float headDisplacement = (head.position - breadcrumbs[0]).magnitude;
 
-		IEnumerator CalcVelocity()
-		{
-			while (Application.isPlaying)
+			if (headDisplacement >= segmentSpacing)
 			{
-				lastPos = transform.position;
-				yield return null;
-				moveSpeed = Mathf.RoundToInt(Vector3.Distance(transform.position, lastPos) / Time.deltaTime);
+				breadcrumbs.RemoveAt(breadcrumbs.Count - 1); 
+				breadcrumbs.Insert(0, head.position); 
+				headDisplacement = headDisplacement % segmentSpacing;
 			}
-		}
 
-		public void Move()
-		{
-			float currSpeed = follower.followSpeed;
-			bodyParts[0].Translate(bodyParts[0].forward * currSpeed * Time.smoothDeltaTime, Space.World);
-
-			for (int i = 1; i < bodyParts.Length; i++)
+			if (headDisplacement != 0)
 			{
-
-				curBodyPart = bodyParts[i];
-				PrevBodyPart = bodyParts[i - 1];
-
-				dis = Vector3.Distance(PrevBodyPart.position, curBodyPart.position);
-
-				Vector3 newpos = PrevBodyPart.position;
-
-				float T = Time.deltaTime * dis / minDistance * currSpeed;
-
-				if (T > 0.5f) T = 0.5f;
-				curBodyPart.position = Vector3.Slerp(curBodyPart.position, newpos, T);
-				curBodyPart.rotation = Quaternion.Slerp(curBodyPart.rotation, PrevBodyPart.rotation, T);
+				Vector3 pos = Vector3.Lerp(breadcrumbs[1], breadcrumbs[0], headDisplacement / segmentSpacing);
+				segments[0].position = pos;
+				segments[0].rotation = Quaternion.Slerp(Quaternion.LookRotation(breadcrumbs[0] - breadcrumbs[1]), Quaternion.LookRotation(head.position - breadcrumbs[0]), headDisplacement / segmentSpacing);
+				
+				for (int i = 1; i < segments.Length; i++)
+				{
+					pos = Vector3.Lerp(breadcrumbs[i + 1], breadcrumbs[i], headDisplacement / segmentSpacing);
+					segments[i].position = pos;
+					segments[i].rotation = Quaternion.Slerp(Quaternion.LookRotation(breadcrumbs[i] - breadcrumbs[i + 1]), Quaternion.LookRotation(breadcrumbs[i - 1] - breadcrumbs[i]), headDisplacement / segmentSpacing);
+				}
 			}
+
 		}
 
-		public void InitiateSlowDown()
-		{
-			isSlowing = true;
-		}
+		// private void SlowDown()
+		// {
+		// 	follower.followSpeed -= slowDownValue;
+		// 	if (follower.followSpeed <= 0)
+		// 	{
+		// 		isSlowing = false;
+		// 		StopMovement();
+		// 	}
+		// }
 
-		private void SlowDown()
-		{
-			follower.followSpeed -= slowDownValue;
-			if(follower.followSpeed <= 0)
-			{
-				isSlowing = false;
-				StopMovement();
-			} 
-		}
+		// public void StopMovement()
+		// {
+		// 	follower.followSpeed = 0;
 
-		public void StopMovement()
-		{
-			follower.followSpeed = 0;
+		// 	overshootJuice.Initialization();
+		// 	overshootJuice.PlayFeedbacks();
+		// }
 
-			overshootJuice.Initialization();
-			overshootJuice.PlayFeedbacks();
-		}
+		// public void ShowSerpent()
+		// {
+		// 	foreach (Transform bodyPart in bodyParts)
+		// 	{
+		// 		bodyPart.GetComponent<MeshRenderer>().enabled = true;
+		// 	}
+		// }
 
-		public void ShowSerpent()
+		private void OnTriggerEnter(Collider other)
 		{
-			foreach (Transform bodyPart in bodyParts)
-			{
-				bodyPart.GetComponent<MeshRenderer>().enabled = true;
-			}
-		}
-
-		private void OnTriggerEnter(Collider other) 
-		{
-			if(other.GetComponent<FinishCube>()) other.transform.parent = transform;
+			if (other.GetComponent<FinishCube>()) other.transform.parent = transform;
 		}
 	}
 }
