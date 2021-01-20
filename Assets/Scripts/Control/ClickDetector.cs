749e5d649ff864df78ab927c2663ed0bf3abd0a7
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2021-01-20 17:28:09 +0100

    ADD: Scripts to make objects clickable with mouse

---
+++ Assets/Scripts/Control/ClickDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDetector : MonoBehaviour
+{
+	//Config parameters
+	[SerializeField] LayerMask layerMask;
+
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+
+			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), 
+				out hit, Mathf.Infinity, layerMask))
+				hit.collider.GetComponent<ClickableObject>().ClickReaction();
+		}
+	}
+}
