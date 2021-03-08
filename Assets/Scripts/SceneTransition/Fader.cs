1a0f1363a46605ad05ced6f874988b44f8b97032
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2021-03-08 11:53:18 +0100

    ADD: Splash scene that loads progresshandler and then loads worldmap ADD: Fader script to be used whenever we fade between scenes ADD: splashsceneloader script EDIT: Proghandler is now in 'persistent object' prefab along with fader EDIT: placed canvas prefabs in UI folder EDIT: Added splashscene and worldmap to build settings again. EDIT: adjusted scene index values in qbismdatasheet accordingly

---
+++ Assets/Scripts/SceneTransition/Fader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Qbism.SceneTransition
+{
+	public class Fader : MonoBehaviour
+	{
+		//Config parameters
+		[SerializeField] float transitionTime;
+
+		//Cache
+		CanvasGroup canvasGroup = null;
+		Coroutine activeCoroutine = null;
+
+		void Awake()
+		{
+			canvasGroup = GetComponentInChildren<CanvasGroup>();
+		}
+
+		public void FadeOutImmediate()
+		{
+			canvasGroup.alpha = 1;
+		}
+
+		public Coroutine FadeOut()
+		{
+			return Fade(1);
+		}
+
+		public Coroutine FadeIn()
+		{
+			return Fade(0);
+		}
+
+		public Coroutine Fade(float target) 
+		{
+			if (activeCoroutine != null) StopCoroutine(activeCoroutine);
+			activeCoroutine = StartCoroutine(FadeRoutine(target)); 
+			return activeCoroutine;
+		}
+
+		private IEnumerator FadeRoutine(float target)
+		{
+			while (!Mathf.Approximately(canvasGroup.alpha, target))
+			{
+				//mathf.movetowards to get float to desired value using desired speed
+				canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, target, Time.deltaTime / transitionTime);
+				yield return null;
+			}
+		}
+	}
+}
