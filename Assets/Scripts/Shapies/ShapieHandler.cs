015ddc7a88373d64771ce7a41f8b5973bf875b37
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2021-04-19 20:36:30 +0200

    EDIT: Organized folders ADD: Shapie (cone) mockup with single eye texture ADD: Shapie pushback animation ADD: ShapieHandler script

---
+++ Assets/Scripts/Shapies/ShapieHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+namespace Qbism.Shapies
+{
+	public class ShapieHandler : MonoBehaviour
+	{
+		//Config parameters
+		[SerializeField] Transform pushBackTarget;
+		[SerializeField] MMFeedbacks pushBackJuice;
+
+		public void pushBack()
+		{
+			pushBackJuice.Initialization();
+			pushBackJuice.PlayFeedbacks();
+		}
+	}
+}
