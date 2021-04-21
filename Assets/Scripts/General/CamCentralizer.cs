1b2c89d60ff5cdc1f8dd99d4c323765cbf09d9fe
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2021-04-21 11:43:50 +0200

    EDIT: camere iso view change EDIT; added crosshair visualizers to centralizer and canvas to help center cam when needed EDIT: rounded shapiecone corners with bevel. re-imported. EDIT: made white_dot higher resolution

--- Assets/Scripts/General/CamCentralizer.cs
+++ Assets/Scripts/General/CamCentralizer.cs
@@ -8,13 +8,21 @@ namespace Qbism.General
 	public class CamCentralizer : MonoBehaviour
 	{
 		//Config parameters
+		[Header("Camera")]
 		[SerializeField] CinemachineVirtualCamera cam = null;
 		[SerializeField] Vector3 camValues = new Vector3 (0, 0, 0);
+		[Header("Debug")]
+		[SerializeField] bool CenterAtUpdate = false;
 
 		void Start()
 		{	
 			PositionCenterpoint posCenter = GetComponent<PositionCenterpoint>();
 			if(!posCenter) PositionCam(); //This is for the level complete cam. Not the gameplay cam.
+		}
+
+		private void Update() 
+		{
+			if (CenterAtUpdate) PositionCam();
 		}
 
 		public void PositionCam()
