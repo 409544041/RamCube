70b9cb511c0ed0ec4db03fd10aad8db438446540
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2021-05-07 15:54:58 +0200

    EDIT: divided MMFeedbacks folder into juice and animation. Moved files accordingly ADD: segment MM Animator script for segment animation at end level sequence ADD: Hop Movement MMFeedbacks prefab for same. Added to Segment 01

---
+++ Assets/Scripts/Serpent/SegmentMMAnimator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+namespace Qbism.Serpent
+{
+	public class SegmentMMAnimator : MonoBehaviour
+	{
+		//Config paramters
+		[SerializeField] MMFeedbacks hopMovement = null;
+		[SerializeField] float hopInterval = 1f;
+
+		//Cache
+		MMFeedbackRotation hopRot = null;
+
+		//States
+		bool firstHop = true;
+
+		private void Awake() 
+		{
+			hopRot = hopMovement.GetComponent<MMFeedbackRotation>();
+		}
+
+		private IEnumerator Start() 
+		{
+			yield return new WaitForSeconds(2f);
+			StartCoroutine(TriggerHopMovement());
+		}
+
+		private IEnumerator TriggerHopMovement()
+		{
+			hopMovement.Initialization();
+			hopMovement.PlayFeedbacks();
+
+			if (firstHop)
+			{
+				firstHop = false;
+				//take the total time of the MMmovement into account for hopInterval duration
+				yield return new WaitForSeconds(hopInterval);
+				hopRot.RemapCurveOne *= -1;
+				StartCoroutine(TriggerHopMovement());
+			}
+		}
+	}
+}
