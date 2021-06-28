02973eba50711cde43dcca59e1d93e6627ce2e5c
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2021-06-28 17:06:21 +0200

    EDIT: changed blending mode from mistways and put them back in levels ADD: 2x wind particles EDIT: mistways now also spawns wind particles

---
+++ Assets/Scripts/Environment/WindSpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Qbism.Environment
+{
+	public class WindSpawner : MonoBehaviour
+	{
+		//Config parameters
+		[SerializeField] Transform[] zMarkers, xMarkers;
+		[SerializeField] ParticleSystem[] windVFX;
+		[SerializeField] Vector2 minMaxSpawnInterval;
+
+		//States
+		float spawnTimer;
+		float timeToSpawn;
+
+		private void Start() 
+		{
+			timeToSpawn = Random.Range(minMaxSpawnInterval.x, minMaxSpawnInterval.y);
+		}
+
+		private void Update()
+		{
+			spawnTimer += Time.deltaTime;
+			SpawnWind();
+		}
+
+		private void SpawnWind()
+		{
+			if (spawnTimer >= timeToSpawn)
+			{
+				spawnTimer = 0;
+				int index = Random.Range(0, windVFX.Length);
+				PositionWind(windVFX[index]);
+				windVFX[index].Play();
+				timeToSpawn = Random.Range(minMaxSpawnInterval.x, minMaxSpawnInterval.y);
+			}
+		}
+
+		private void PositionWind(ParticleSystem windVFX)
+		{
+			var spawnPosZ = Random.Range(zMarkers[0].position.z, zMarkers[1].position.z);
+			var spawnPosX = Random.Range(xMarkers[0].position.x, xMarkers[1].position.x);
+			Vector3 spawnPos = new Vector3(spawnPosX, transform.position.y, spawnPosZ);
+			windVFX.transform.position = spawnPos;
+		}
+	}
+}
