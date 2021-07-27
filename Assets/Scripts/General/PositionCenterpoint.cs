34d5be9d2a4e0091caca9a6fcc97d9c590d521c6
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2021-07-27 12:35:00 +0200

    ADD: prefab for gameplay cinemachine cam EDIT: cam now slightly follows player while keeping rest of level in view using cinemachine framing transposer

--- Assets/Scripts/General/PositionCenterpoint.cs
+++ Assets/Scripts/General/PositionCenterpoint.cs
@@ -7,31 +7,51 @@ namespace Qbism.General
 {
 	public class PositionCenterpoint : MonoBehaviour
 	{
+		//Config parameters
+		[SerializeField] bool isDynamicCam;
+
+		//Cache
+		CubeHandler handler;
+
 		//States
 		float highest, lowest, leftest, rightest;
-		bool firstValueAssigned = false;
+		bool firstValueAssigned;
 		Vector3 centerPoint = new Vector3(0, 0, 0);
-		
-		void Start()
+		Vector2 highestCube, lowestCube, leftestCube, rightestCube;
+
+		private void Awake() 
 		{
+			handler = FindObjectOfType<CubeHandler>();
+		}
+		
+		private void Start()
+		{
+			FindEdges();
 			PositionCenterPoint();
+		}
+
+		private void Update() 
+		{
+			if (isDynamicCam) 
+			{
+				FindEdgeCubes();
+				PositionCenterPoint();
+			}
 		}
 
 		private void PositionCenterPoint()
 		{
-			FindEdges();
-			CalculateCenterpoint();
-
 			transform.position = centerPoint;
 
-			CamCentralizer centralizer = GetComponent<CamCentralizer>();
-			if(centralizer) centralizer.PositionCam();
+			if (!isDynamicCam)
+			{
+				CamCentralizer centralizer = GetComponent<CamCentralizer>();
+				if (centralizer) centralizer.PositionCam();
+			}
 		}
 
 		private void FindEdges()
 		{
-			CubeHandler handler = FindObjectOfType<CubeHandler>();
-
 			foreach (KeyValuePair<Vector2Int, FloorCube> cube in handler.floorCubeDic)
 			{
 				Vector2 viewPortPos = Camera.main.WorldToViewportPoint(cube.Value.transform.position);
@@ -50,7 +70,38 @@ namespace Qbism.General
 				if (viewPortPos.y < lowest) lowest = viewPortPos.y;
 				if (viewPortPos.x < leftest) leftest = viewPortPos.x;
 				if (viewPortPos.x > rightest) rightest = viewPortPos.x;
+
+				CalculateCenterpoint();
 			}
+
+			firstValueAssigned = false;
+		}
+
+		private void FindEdgeCubes()
+		{
+			foreach (KeyValuePair<Vector2Int, FloorCube> cube in handler.floorCubeDic)
+			{
+				Vector2 viewPortPos = Camera.main.WorldToViewportPoint(cube.Value.transform.position);
+
+				if (!firstValueAssigned)
+				{
+					highestCube = viewPortPos;
+					lowestCube = viewPortPos;
+					leftestCube = viewPortPos;
+					rightestCube = viewPortPos;
+
+					firstValueAssigned = true;
+				}
+
+				if (viewPortPos.y > highestCube.y) highestCube = viewPortPos;
+				if (viewPortPos.y < lowestCube.y) lowestCube = viewPortPos;
+				if (viewPortPos.x < leftestCube.x) leftestCube = viewPortPos;
+				if (viewPortPos.x > rightestCube.x) rightestCube = viewPortPos;
+
+				CalculateAvgPoint(highestCube, lowestCube, leftestCube, rightestCube);
+			}
+
+			firstValueAssigned = false;
 		}
 
 		private void CalculateCenterpoint()
@@ -61,7 +112,31 @@ namespace Qbism.General
 			float xPos = leftest + (deltaX / 2);
 			float yPos = lowest + (deltaY / 2);
 
-			centerPoint = Camera.main.ViewportToWorldPoint(new Vector3(xPos, yPos, 10));
+			centerPoint = Camera.main.ViewportToWorldPoint(new Vector3(xPos, yPos, 15)); 
+			centerPoint = new Vector3(centerPoint.x, 0, centerPoint.z);
+		}
+
+		private void CalculateAvgPoint(Vector2 highCube, Vector2 lowCube, Vector2 leftCube, Vector2 rightCube)
+		{
+			GameObject player = GameObject.FindGameObjectWithTag("Player");
+			Vector2 PlayerPortPos = Camera.main.WorldToViewportPoint(player.transform.position);
+
+			Vector2[] PortPositions = {highCube, lowCube, leftCube, rightCube, PlayerPortPos};
+
+			var totalX = 0f;
+			var totalY = 0f;
+
+			foreach (var pos in PortPositions)
+			{
+				totalX += pos.x;
+				totalY += pos.y;
+			}
+
+			var centerX = totalX / PortPositions.Length;
+			var centerY = totalY / PortPositions.Length;
+
+			centerPoint = Camera.main.ViewportToWorldPoint(new Vector3(centerX, centerY, 15));
+			centerPoint = new Vector3(centerPoint.x, 0, centerPoint.z);
 		}
 	}
 }
