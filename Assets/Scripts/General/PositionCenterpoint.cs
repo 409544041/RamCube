341446665977ff8ac224f26533f562350d0f93d4
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2021-07-27 10:45:20 +0200

    EDIT: No code change, just tabs

--- Assets/Scripts/General/PositionCenterpoint.cs
+++ Assets/Scripts/General/PositionCenterpoint.cs
@@ -32,36 +32,36 @@ namespace Qbism.General
 		{
 			CubeHandler handler = FindObjectOfType<CubeHandler>();
 
-				foreach (KeyValuePair<Vector2Int, FloorCube> cube in handler.floorCubeDic)
-				{
-					Vector2 viewPortPos = Camera.main.WorldToViewportPoint(cube.Value.transform.position);
-
-					if (!firstValueAssigned)
-					{
-						highest = viewPortPos.y;
-						lowest = viewPortPos.y;
-						leftest = viewPortPos.x;
-						rightest = viewPortPos.x;
-
-						firstValueAssigned = true;
-					}
-
-					if (viewPortPos.y > highest) highest = viewPortPos.y;
-					if (viewPortPos.y < lowest) lowest = viewPortPos.y;
-					if (viewPortPos.x < leftest) leftest = viewPortPos.x;
-					if (viewPortPos.x > rightest) rightest = viewPortPos.x;
-				}
-			}
-
-			private void CalculateCenterpoint()
+			foreach (KeyValuePair<Vector2Int, FloorCube> cube in handler.floorCubeDic)
 			{
-				float deltaX = rightest - leftest;
-				float deltaY = highest - lowest;
+				Vector2 viewPortPos = Camera.main.WorldToViewportPoint(cube.Value.transform.position);
 
-				float xPos = leftest + (deltaX / 2);
-				float yPos = lowest + (deltaY / 2);
+				if (!firstValueAssigned)
+				{
+					highest = viewPortPos.y;
+					lowest = viewPortPos.y;
+					leftest = viewPortPos.x;
+					rightest = viewPortPos.x;
 
-				centerPoint = Camera.main.ViewportToWorldPoint(new Vector3(xPos, yPos, 10));
+					firstValueAssigned = true;
+				}
+
+				if (viewPortPos.y > highest) highest = viewPortPos.y;
+				if (viewPortPos.y < lowest) lowest = viewPortPos.y;
+				if (viewPortPos.x < leftest) leftest = viewPortPos.x;
+				if (viewPortPos.x > rightest) rightest = viewPortPos.x;
 			}
+		}
+
+		private void CalculateCenterpoint()
+		{
+			float deltaX = rightest - leftest;
+			float deltaY = highest - lowest;
+
+			float xPos = leftest + (deltaX / 2);
+			float yPos = lowest + (deltaY / 2);
+
+			centerPoint = Camera.main.ViewportToWorldPoint(new Vector3(xPos, yPos, 10));
+		}
 	}
 }
