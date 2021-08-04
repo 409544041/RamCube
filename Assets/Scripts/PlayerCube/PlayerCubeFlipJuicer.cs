2ff790e1fe540846cb05e65a98a34bed4cb16ddd
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2021-08-04 10:53:30 +0200

    FIX: bug where post flip juice reposition values were off bc it's now set on local.

--- Assets/Scripts/PlayerCube/PlayerCubeFlipJuicer.cs
+++ Assets/Scripts/PlayerCube/PlayerCubeFlipJuicer.cs
@@ -17,12 +17,22 @@ namespace Qbism.PlayerCube
 		AudioSource source;
 		MMFeedbackScale[] postFlipMMScalers;
 		MMFeedbackScale[] flipMMScalers;
+		MMFeedbackPosition postFlipMMPos;
+
+		//States
+		float postFlipCurveOneAtStart = 0;
 
 		private void Awake() 
 		{
 			source = GetComponentInChildren<AudioSource>();
 			postFlipMMScalers = postFlipJuice.GetComponents<MMFeedbackScale>();
 			flipMMScalers = flipJuice.GetComponents<MMFeedbackScale>();
+			postFlipMMPos= postFlipJuice.GetComponent<MMFeedbackPosition>();
+		}
+
+		private void Start() 
+		{
+			postFlipCurveOneAtStart = postFlipMMPos.RemapCurveOne;
 		}
 
 		public void PlayFlipJuice()
@@ -42,6 +52,8 @@ namespace Qbism.PlayerCube
 			{
 				CalculateFlipScaleAxis(i, postFlipMMScalers);
 			}
+
+			CalculateScaleReposDir();
 
 			postFlipJuice.Initialization();
 			postFlipJuice.PlayFeedbacks();
@@ -78,12 +90,43 @@ namespace Qbism.PlayerCube
 			}
 		}
 
+		//below needs to be done bc we're repositioning this MMJuice local and not world
+		private void CalculateScaleReposDir()
+		{
+			if (V3Equal(transform.forward, Vector3.up))
+				SetScaleReposValues(false, false, true, 1);
+
+			if (V3Equal(-transform.forward, Vector3.up))
+				SetScaleReposValues(false, false, true, -1);
+
+			if (V3Equal(transform.up, Vector3.up))
+				SetScaleReposValues(false, true, false, 1);
+
+			if (V3Equal(-transform.up, Vector3.up))
+				SetScaleReposValues(false, true, false, -1);
+
+			if (V3Equal(transform.right, Vector3.up))
+				SetScaleReposValues(true, false, false, 1);
+
+			if (V3Equal(-transform.right, Vector3.up))
+				SetScaleReposValues(true, false, false, -1);
+		}
+
 		private void SetFlipScaleValues(int i, MMFeedbackScale[] scalers,
 			bool xValue, bool yValue, bool zValue)
 		{
 			scalers[i].AnimateX = xValue;
 			scalers[i].AnimateY = yValue;
 			scalers[i].AnimateZ = zValue;
+		}
+
+		private void SetScaleReposValues(bool xValue, bool yValue, bool zValue, int curveMult)
+		{
+			postFlipMMPos.RemapCurveOne = postFlipCurveOneAtStart;
+			postFlipMMPos.RemapCurveOne *= curveMult;
+			postFlipMMPos.AnimateX = xValue;
+			postFlipMMPos.AnimateY = yValue;
+			postFlipMMPos.AnimateZ = zValue;
 		}
 
 		private bool IsPlayerZWorldY()
