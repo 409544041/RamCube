dcfbd6508d2e6474ef814ab58e910b9935a3cfbf
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2021-08-25 14:11:17 +0200

    ADD: segment04 spritesheets, anim, controller overrider etc EDIT: Renamed shape in shapies EDIT: gave segment04 prefab correct face EDIT: tweak on promocontroller and animforcer

--- Assets/Scripts/Promo/ShapieAnimForcer.cs
+++ Assets/Scripts/Promo/ShapieAnimForcer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Qbism.Shapies;
 using UnityEngine;
 
 namespace Qbism.Promo
@@ -8,17 +9,16 @@ namespace Qbism.Promo
 	{
 		//Config parameters
 		[SerializeField] Vector2 minMaxLookDelay, minMaxCelebrateDelay;
-		[SerializeField] bool forceCelebration, forceLook;
-		[Range(0,1)]
-		[SerializeField] int celebAnim;
-		
+		[SerializeField] bool forceCeleb01, forceCeleb02, forceLook;		
 
 		//Cache
 		Animator animator;
+		ShapieSoundHandler soundHandler;
 
 		private void Awake()
 		{
 			animator = GetComponent<Animator>();
+			soundHandler = GetComponentInParent<ShapieSoundHandler>();
 		}
 
 		public void ForceLookingAround()
@@ -30,19 +30,26 @@ namespace Qbism.Promo
 		{
 			var delay = Random.Range(minMaxLookDelay.x, minMaxLookDelay.y);
 			yield return new WaitForSeconds(delay);
-			animator.SetTrigger("LookAround");
+			animator.SetTrigger("Look");
 		}
 
 		public void ForceCelebrate()
 		{
-			if (forceCelebration) StartCoroutine(DebugCelebrate());
+			if (forceCeleb01 || forceCeleb02) StartCoroutine(DebugCelebrate());
 		}
 
 		private IEnumerator DebugCelebrate()
 		{
 			var delay = Random.Range(minMaxCelebrateDelay.x, minMaxCelebrateDelay.y);
 			yield return new WaitForSeconds(delay);
-			animator.SetInteger("Celebrate", celebAnim);
+			if (forceCeleb01) animator.SetTrigger("Celebrate01");
+			else if (forceCeleb02) animator.SetTrigger("Celebrate02");
+		}
+
+		private void TriggerGibberishCelebration()
+		{
+			soundHandler.currentIntervalMinMax = soundHandler.fastIntervalMinMax;
+			soundHandler.PlayGibberish(true);
 		}
 	}
 }
