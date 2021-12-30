2fbcb64aa5346d639012b5d6dfe666f52ccb4be8
jonathan <j.s.barbosa.dijkstra@gmail.com>
2021-12-30 15:25:31 +0100

    EDIT: Removed cam placer on finish cube end cam, using cinemachine framing composer instead EDIT: increased distance of finish end cam to allow space for dialogue stuff inbetween EDIT: added dialogue starter to head ADD: Dialogue canvasses with mockup/wireframe visuals EDIT: DIalogue canvas gets triggered and first text from dialogueSO linked in database shows correctly

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,29 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace Qbism.Dialogue
 {
 	public class DialogueManager : MonoBehaviour
 	{
 		//Config parameters
-		[SerializeField] Canvas dialogueCanvas;
-		[SerializeField] Transform[] floatingHeadPos;
+		[SerializeField] Canvas dialogueCanvas, backgroundCanvas;
+		[SerializeField] TextMeshProUGUI charNameText, dialogueText;
+		[SerializeField] Vector3[] floatingHeadPos;
 		[SerializeField] float headScale;
 
 		//States
 		DialogueScripOb dialogueSO;
 		int dialogueIndex = 0;
+		bool inDialogue = false;
 
 		public void StartDialogue(ScriptableObject incDialogueSO, Object floatingHead, Vector3 headRot)
 		{
 			dialogueSO = (DialogueScripOb)incDialogueSO;
 			print("Starting a convo lol");
-			// remove input control over gameplay if it isn't already removed
+
 			// show canvas
+			SetupBackgroundCanvas();
+			dialogueCanvas.GetComponent<CanvasGroup>().alpha = 1;
+
+
+			// remove input control over gameplay if it isn't already removed
+
 			// place floating heads at correct location
+
+
 			// inDialogue = true (a check for input control)
+			inDialogue = true;
+
 			// printDialogue first dialogue text
+			var charIndex = dialogueSO.dialogues[dialogueIndex].characterSpeaking;
+			charNameText.text = dialogueSO.characters[charIndex].ToString();
+			dialogueText.text = dialogueSO.dialogues[dialogueIndex].dialogueText;
+		}
+
+		private void SetupBackgroundCanvas()
+		{
+			backgroundCanvas.transform.parent = Camera.main.transform;
+			backgroundCanvas.transform.rotation = Camera.main.transform.rotation;
+			backgroundCanvas.transform.localPosition = new Vector3(0, 0, 10);
+			backgroundCanvas.GetComponent<CanvasGroup>().alpha = 1;
 		}
 
 		public void NextDialogueText()
