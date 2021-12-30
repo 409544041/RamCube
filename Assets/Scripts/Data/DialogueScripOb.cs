6684c7cebe1ab44fedd649e93546af357e7f91ff
jonathan <j.s.barbosa.dijkstra@gmail.com>
2021-12-30 12:23:09 +0100

    EDIT: Moved segments to resources folder so they can be used in database ADD: Dialogue manager ADD: Dialogue scriptable object ADD: Segments database ADD: Dialogues database EDIT: commented to do for dialogue system. Nothing works yet though

---
+++ Assets/Scripts/Data/DialogueScripOb.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue Scip Obj", menuName = "ScriptableObjects/Dialogue")]
+
+public class DialogueScripOb : ScriptableObject
+{
+	//Config paramters
+	public SegmentIDs[] characters;
+	public DialogueData[] dialogues;
+
+	[System.Serializable]
+	public class DialogueData
+	{
+		public int characterSpeaking;
+		public Expressions expression;
+		[TextArea(3, 10)]
+		public string dialogueText;
+	}
+}
---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Qbism.Dialogue
+{
+	public class DialogueManager : MonoBehaviour
+	{
+		//Config parameters
+		[SerializeField] Canvas dialogueCanvas;
+		[SerializeField] Vector3[] floatingHeadPos;
+
+		//States
+		DialogueScripOb dialogueSO;
+		int dialogueIndex = 0;
+
+		public void StartDialogue(DialogueScripOb incDialogueSO)
+		{
+			//dialogueSO = incDialogueSO
+			// remove input control over gameplay if it isn't already removed
+			// show canvas
+			// place floating heads at correct location
+			// inDialogue = true (a check for input control)
+			// printDialogue first dialogue text
+		}
+
+		public void NextDialogueText()
+		{
+			// dialogueindex++
+			// if dialogue index >= dialogueSO.dialogues length, ExitDialogue
+			// else printDialogue next dialogueSO.dialogues
+
+		}
+
+		private void PrintDialogue()
+		{
+			// canvas text = dialogueSO text
+		}
+
+		private void ExitDialogue()
+		{
+			// turn off dialogue canvas
+			// switch player controls back using inDialogue = false
+			// find a way to set played = true in dialogues database
+		}
+
+	}
+}
