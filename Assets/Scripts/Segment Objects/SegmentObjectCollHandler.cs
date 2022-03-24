24653dee006d3330bf13739eb2fd4c62807400bd
jonathan <j.s.barbosa.dijkstra@gmail.com>
2022-03-24 13:56:58 +0100

    ADD: SegmentObjCollHandler. seperated collision script from juicer into own script

---
+++ Assets/Scripts/Segment Objects/SegmentObjectCollHandler.cs
@@ -0,0 +1,32 @@
+using MoreMountains.Feedbacks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Qbism.Objects
+{
+	public class SegmentObjectCollHandler : MonoBehaviour
+	{
+		[SerializeField] MMFeedbacks swallowAndFartOutJuice;
+		[SerializeField] SegObjectRefHolder refs;
+
+		//States
+		bool swallowFartJuiceActivated = false;
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if (other.tag != "PlayerFace") return;
+			if (swallowFartJuiceActivated) return;
+
+			swallowAndFartOutJuice.PlayFeedbacks();
+			swallowFartJuiceActivated = true;
+
+			var objCollManager = FindObjectOfType<ObjectCollectManager>();
+			objCollManager.collectableObject = this.gameObject;
+			objCollManager.objJuicer = refs.juicer;
+			objCollManager.objMesh = refs.mesh;
+			objCollManager.m_Object = refs.m_objects;
+			objCollManager.InitiateShowingObjectOverlay();
+		}
+	}
+}
--- Assets/Scripts/Segment Objects/SegmentObjectJuicer.cs
+++ Assets/Scripts/Segment Objects/SegmentObjectJuicer.cs
@@ -8,31 +8,10 @@ namespace Qbism.Objects
 	public class SegmentObjectJuicer : MonoBehaviour
 	{
 		//Config parameters
-		[SerializeField] MMFeedbacks swallowAndFartOutJuice, scaleUpJuice, starScaleJuice;
+		[SerializeField] MMFeedbacks scaleUpJuice, starScaleJuice;
 		[SerializeField] float scaleUpOvershoot = .6f;
-		[SerializeField] Renderer mesh;
-		[SerializeField] M_Objects m_Object;
 		public ParticleSystem fartParticles;
 		public Canvas uiStar;
-
-		//States
-		bool swallowFartJuiceActivated = false;
-
-		private void OnTriggerEnter(Collider other)
-		{
-			if (other.tag != "PlayerFace") return;
-			if (swallowFartJuiceActivated) return;
-
-			swallowAndFartOutJuice.PlayFeedbacks();
-			swallowFartJuiceActivated = true;
-
-			var objCollManager = FindObjectOfType<ObjectCollectManager>();
-			objCollManager.collectableObject = this.gameObject;
-			objCollManager.objJuicer = this;
-			objCollManager.objMesh = mesh;
-			objCollManager.m_Object = m_Object;
-			objCollManager.InitiateShowingObjectOverlay();
-		}
 
 		public void TriggerScaleUpJuice()
 		{
