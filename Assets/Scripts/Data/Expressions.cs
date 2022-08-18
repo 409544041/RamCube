85503572c9b5013979bfc8bb099aa9bbf0a15945
jonathan <j.s.barbosa.dijkstra@gmail.com>
2022-08-18 17:54:19 +0200

    EDIT: Surprised expression aan billy toegevoegd

--- Assets/Scripts/Data/Expressions.cs
+++ Assets/Scripts/Data/Expressions.cs
@@ -2,5 +2,5 @@ public enum Expressions
 {
 	neutral, smiling, veryHappy, gleeful, laughing, hysterical, toothyLaugh, guiltySmile,
 	sad, ouch, painful, annoyed, angry, mischievous, calm, twitch, frown, shocked, gushing, 
-	pushing, looking, blink, boop, yawn, archSmile
+	pushing, looking, blink, boop, yawn, archSmile, surprised
 }
